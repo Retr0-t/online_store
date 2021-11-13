90ac3af44a68378499fb78b39e1f682e078f153a
mighty <hrandika17@gmail.com>
2021-11-13 09:34:30 +0700

    ganti judul

--- onlinestore.php
+++ onlinestore.php
@@ -4,7 +4,7 @@ session_start();
 <!DOCTYPE html>
 
 <head>
-	<title> STORE</title>
+	<title>ONLINE STORE</title>
 		<link rel="stylesheet" href="assets/css/bootstrap.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="assets/js/bootstrap.js "></script>
